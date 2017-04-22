f2672ad3597f06379fe28a386b6df6ed0408757b
Yusuf Yiğit Pilavcı <yigit.pilavci01@gmail.com>
2017-04-23 00:57:57 +0300

    hey

--- app22bostan.ino
+++ app22bostan.ino
@@ -216,7 +216,7 @@ void Motordrive(int dir2, float velocity_L, float velocity_R){
  if (velocity_L >255)velocity_L=255;
  if (velocity_R >255)velocity_R=255;
 // MOTORLAR BURADA SUSTURULDU
-/*
+
  if (dir2) { //not yapılacak ipi kontrol ediyor
   analogWrite(Mr1,velocity_L);
   analogWrite(Mr2,velocity_R*0.97);
@@ -229,7 +229,6 @@ else {
   analogWrite(Mr1,0);
   analogWrite(Mr2,0);
   }
-*/
 }
 /*
   SerialEvent occurs whenever a new data comes in the
